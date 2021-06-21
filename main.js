9e1991c12c1756292ee312ae586fdab497b4fa79
Borges Camilla <camillaborgeshelena@gmail.com>
2021-06-21 10:07:32 +0200

    implementing functionality to the game. blocking passage through walls

--- main.js
+++ main.js
@@ -44,7 +44,9 @@ function createBoard() {
             squares[i].classList.add("pac-dot")
         } else if(layout[i] === 1) {
             squares[i].classList.add("wall")
-        }else if(layout[i] === 3) {
+        } else if(layout[i] === 2) { 
+            squares[i].classList.add("ghost-lair")
+        } else if(layout[i] === 3) {
             squares[i].classList.add("power-pellet") 
         }
     }
@@ -56,19 +58,51 @@ let pacmanCurrentIndex = 490
 squares[pacmanCurrentIndex].classList.add("pacman")
 
 function control(e) {
+    squares[pacmanCurrentIndex].classList.remove("pacman")
     switch(e.keyCode) {
         case 40:
             console.log("pressed down")
+            if (
+                !squares[pacmanCurrentIndex + width].classList.contais("ghost-lair") &&
+                !squares[pacmanCurrentIndex + width].classList.contains("wall") &&
+                pacmanCurrentIndex + width < width * width
+                ) 
+                pacmanCurrentIndex += width
             break;
         case 38:
             console.log("pressed up")
+            if (
+                !squares[pacmanCurrentIndex - width].classList.contains("ghost-lair") &&
+                !squares[pacmanCurrentIndex - width].classList.contains("wall") &&
+                pacmanCurrentIndex - width >=0
+                ) 
+                pacmanCurrentIndex -= width
             break;
         case 37:
             console.log("pressed left")
+            if (
+                !squares[pacmanCurrentIndex -1].classList.contains("ghost-lair") &&
+                !squares[pacmanCurrentIndex -1].classList.contains("wall") &&
+                pacmanCurrentIndex % width !== 0
+                ) 
+                pacmanCurrentIndex -=1
+                if (pacmanCurrentIndex === 364) {
+                    pacmanCurrentIndex = 391
+                }
             break;
         case 39:
             console.log("pressed right")
+            if(
+                !squares[pacmanCurrentIndex +1].classList.contains("ghost-lair") &&
+                !squares[pacmanCurrentIndex +1].classList.contains("wall") &&
+                pacmanCurrentIndex % width < width -1
+                ) 
+                pacmanCurrentIndex +=1
+                if (pacmanCurrentIndex === 391) {
+                    pacmanCurrentIndex = 364
+                }
     }
+    squares[pacmanCurrentIndex].classList.add("pacman")
 }
 
 document.addEventListener("keyup", control)
